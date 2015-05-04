5b5c56891ef5d246d7c5def77a2099f76c9b5056
Jonas Lindeløv <jonas@cnru.dk>
2015-05-04 23:51:00 +0200

    Updated coding style and removed iohub dependency.

--- ppc_template.py
+++ ppc_template.py
@@ -7,7 +7,7 @@ http://lindeloev.net/?page_id=134
 It does require that you have the ppc.py in the same folder as the script.
 Find ppc.py on the course website.
 
-Version 2014-02
+Version 2015-05
 Free to use for any purpose. Credit is nice but not required.
 By Jonas Kristoffer Lindeløv: jonas@cnru.dk
 
@@ -29,7 +29,7 @@ THE EXPERIMENT:
 This is a silly experiment in which users are asked to indicate the orientation
 of a gabor patch. It can show up at different locations and is either preceeded
 by a fixation cross indicating it's true position or a false position.
-Furthermore, it has various durations. Maybe we wan't to see the impact of cue
+Furthermore, it has various durations. Maybe we want to see the impact of cue
 and stimulus duration on identification performance.
 
 Formally the design is factorial:
@@ -52,6 +52,8 @@ experiments will use:
     * Use eye-degrees as units of visual size.
     * Save data after each trial to minimize data loss in the event of a crash.
     * Easily scalable to complex experiments.
+    * Note that iohub should be used to get keyboard events in general, but in 
+      this experiment, event is fine since all answers are during wait-periods.
 
 CODE STRATEGY / PHILOSOPHY
 The general coding strategy is to prepare absolutely everything before the
@@ -90,35 +92,35 @@ The overall code structure of this template goes like this:
 SET VARIABLES
 """
 # General variables
-monDistance = 60                 # Distance between subject's eyes and monitor
-monWidth = 50                    # Width of your monitor in cm
-monitorSizePix = [1024, 768]     # Pixel-dimensions of your monitor
-saveFolder = 'templateData'      # Log is saved to this folder. The folder is created if it does not exist.
+mon_distance = 60  # Distance between subject's eyes and monitor
+mon_width = 50  # Width of your monitor in cm
+mon_size = [1024, 768]  # Pixel-dimensions of your monitor
+save_folder = 'templateData'  # Log is saved to this folder. The folder is created if it does not exist.
 
 # Setting up stimulus
-stimSF = 4          # 4 cycles per degree visual angle
-stimOpacity = 0.5   # 50 %
-stimSize = 5        # in degrees visual angle
-fixHeight = 1       # Text height of fixation cross
+gabor_sf = 4  # 4 cycles per degree visual angle
+gabor_opacity = 0.5  # 50 %
+gabor_size = 5  # in degrees visual angle
+fix_height = 1  # Text height of fixation cross
 
-framesFix = 30      # in frames. ~ 500 ms on 60 Hz
-framesStim = [6, 9, 12]     # in frames. ~ 100, 150 and 200 ms on 60 Hz
-framesMask = 3      # in frames. ~ 50 ms on 60 Hz
+frames_fix = 30  # in frames. ~ 500 ms on 60 Hz
+frames_stim = [6, 9, 12]  # in frames. ~ 100, 150 and 200 ms on 60 Hz
+frames_mask = 3  # in frames. ~ 50 ms on 60 Hz
 
 # Trial parameters
-repetitions = 2               # number of trials per condition
-positions = [-3, 0, 3]        # x-positions
+repetitions = 2  # number of trials per condition
+positions = [-3, 0, 3]  # x-positions
 oris = {'right': 0, 'left': 90}  # Orientations and corresponding responses! values = oris.values() and keyboard keys = oris.keys()
-pauseInterval = 10            # Number of trials between breaks
+pause_interval = 10  # Number of trials between breaks
 
 # Questions and messages
-messagePos = [0, 3]  # [x, y]
-messageHeight = 1  # Height of the text, still in degrees visual angle
-textBreak = 'Press SPACE to continue...'  # text of regular break
-keysBreak = [' ', 'space']  # Keys to accept at regular break
-keysQuit = ['escape']  # Keys that quits the experiment
+message_pos = [0, 3]  # [x, y]
+message_height = 1  # Height of the text, still in degrees visual angle
+text_break = 'Press SPACE to continue...'  # text of regular break
+keys_break = [' ', 'space']  # Keys to accept at regular break
+keys_quit = ['escape']  # Keys that quits the experiment
 
-textInstruct = """
+text_instruct = """
     Press LEFT if the lines are horizontal.
     Press RIGHT if the lines are vertical.
 
@@ -129,16 +131,13 @@ textInstruct = """
  SHOW DIALOGUE AND INITIATE PSYCHOPY STIMULI
  This is computationally heavy stuff. Thus we do it in the beginning of our experiment
 """
-# Set python version explicitly for replicability before loading submodules.
-import psychopy
-psychopy.useVersion('1.82.01')
 
 # Import stuff
 import ppc
-print 'the physical diameter of the gabor patch should be', ppc.deg2cm(stimSize, monDistance), 'cm'
-print 'the physical size of the fixation cross should be', ppc.deg2cm(fixHeight, monDistance), 'cm'
+print 'the physical diameter of the gabor patch should be', ppc.deg2cm(gabor_size, mon_distance), 'cm'
+print 'the physical size of the fixation cross should be', ppc.deg2cm(fix_height, mon_distance), 'cm'
 
-from psychopy import core, visual, gui, monitors, sound
+from psychopy import core, visual, gui, monitors, sound, event
 import random
 
 # Intro-dialogue. Get subject-id and other variables.
@@ -149,51 +148,50 @@ if not gui.DlgFromDict(V, order=['subject', 'age', 'gender']).OK:
 
 # Stuff
 clock = core.Clock()  # A clock wich will be used throughout the experiment to time events on a trial-per-trial basis (stimuli and reaction times).
-writer = ppc.csvWriter(str(V['subject']), saveFolder=saveFolder) # writer.write(trial) will write individual trials with low latency
-keyboard = ppc.Keyboard()  # keyboard is now initiated for response collection
+writer = ppc.csvWriter(str(V['subject']), saveFolder=save_folder)  # writer.write(trial) will write individual trials with low latency
 
 # Create psychopy window
-myMonitor = monitors.Monitor('testMonitor', width=monWidth, distance=monDistance)  # Create monitor object from the variables above. This is needed to control size of stimuli in degrees.
-myMonitor.setSizePix(monitorSizePix)
-win = visual.Window(monitor=myMonitor, units='deg', fullscr=True, allowGUI=False, color='black')  # Initiate psychopy Window as the object "win", using the myMon object from last line. Use degree as units!
+my_monitor = monitors.Monitor('testMonitor', width=mon_width, distance=mon_distance)  # Create monitor object from the variables above. This is needed to control size of stimuli in degrees.
+my_monitor.setSizePix(mon_size)
+win = visual.Window(monitor=my_monitor, units='deg', fullscr=True, allowGUI=False, color='black')  # Initiate psychopy Window as the object "win", using the myMon object from last line. Use degree as units!
 
 # Stimuli.
-stimGabor = visual.GratingStim(win, mask='gauss', sf=stimSF, opacity=stimOpacity, size=stimSize)  # A gabor patch. Again, units are inherited.
-stimFix = visual.TextStim(win, '+', height=fixHeight)  # Fixation cross is just the character "+". Units are inherited from Window when not explicitly specified.
-stimText = visual.TextStim(win, pos=messagePos, height=messageHeight, wrapWidth=999)  # Message / question stimulus. Will be used to display instructions and questions.
-beepSuccess = sound.SoundPyo('C', secs=0.1, octave=6)  # Obs, ppc.Sound() is much more accurate, but only works on windows.
-beepFail = sound.SoundPyo('C', secs=0.4, octave=4)
+stim_gabor = visual.GratingStim(win, mask='gauss', sf=gabor_sf, opacity=gabor_opacity, size=gabor_size)  # A gabor patch. Again, units are inherited.
+stim_fix = visual.TextStim(win, '+', height=fix_height)  # Fixation cross is just the character "+". Units are inherited from Window when not explicitly specified.
+stim_text = visual.TextStim(win, pos=message_pos, height=message_height, wrapWidth=999)  # Message / question stimulus. Will be used to display instructions and questions.
+sound_success = sound.Sound('C', secs=0.1, octave=6)  # Obs, ppc.Sound() is much more accurate, but only works on windows.
+sound_fail = sound.Sound('C', secs=0.4, octave=4)
+
 
 """
  FUNCTIONS
 """
 
-def ask(text='', keyList=[]):
+def ask(text='', keyList=None):
     """
     Ask subject something. Shows question and returns answer (keypress)
     and reaction time. Defaults to no text and all keys.
     """
     # Draw the TextStims to visual buffer, then show it and reset timing immediately (at stimulus onset)
-    stimText.setText(text)
-    stimText.draw()
-    win.flip()
-    keyboard.reset()
+    stim_text.setText(text)
+    stim_text.draw()
+    time_flip = win.flip()  # time of core.monotonicClock.getTime() at flip
 
     # Halt everything and wait for (first) responses matching the keys given in the Q object.
     if keyList:
-        keyList += keysQuit
-    response = keyboard.waitKeys(keyList=keyList)
-    if response[0]['key'] in keysQuit:  # Look at first reponse [0]. Quit everything if quit-key was pressed
+        keyList += keys_quit
+    response = event.waitKeys(keyList=keyList, timeStamped=True)  # timestamped according to core.monotonicClock.getTime() at keypress
+    if response[0][0] in keys_quit:  # Look at first reponse [0]. Quit everything if quit-key was pressed
         core.quit()
-    return response[0]['key'], response[0]['tDown']  # When answer given, return it.
+    return response[0][0], response[0][1] - time_flip  # When answer given, return it.
 
 
-def makeTrialList(condition):
+def make_trial_list(condition):
     """
     Return a list of trials (list of dictionaries) in advance of actually displaying them.
     A makeTriallList('falseFix') with repetitions=1 could generate a trial list like this::
 
-    trialList = [
+    trial_list = [
          {'xpos': 0, 'ori': 90, 'durationReal': '', 'response': '', 'condition': 'falseFix', 'subject': u'nick', 'rt': '', 'no': 0, 'gender': u'male', 'age': u'42', 'score': '', 'fixPos': -3},
          {'xpos': -3, 'ori': 90, 'durationReal': '', 'response': '', 'condition': 'falseFix', 'subject': u'nick', 'rt': '', 'no': 1, 'gender': u'male', 'age': u'42', 'score': '', 'fixPos': 0},
          {'xpos': -3, 'ori': 0, 'durationReal': '', 'response': '', 'condition': 'falseFix', 'subject': u'nick', 'rt': '', 'no': 2, 'gender': u'male', 'age': u'42', 'score': '', 'fixPos': 3},
@@ -205,7 +203,7 @@ def makeTrialList(condition):
     Calling trials[0] gives the first trial, trials[1] the 2nd trial etc.
     When the experiment executes, it will simply loop over these trials from first to last using
 
-    for trial in trialList:
+    for trial in trial_list:
         # do something with the trial here
 
     It is suggested to keep all non-constant information in trials instead of
@@ -222,13 +220,13 @@ def makeTrialList(condition):
     """
 
     # Factorial design
-    trialList = []
+    trial_list = []
     for ori in oris.values():
         for pos in positions:
-            for dur in framesStim:
+            for dur in frames_stim:
                 for rep in range(repetitions):
                     # Add a dictionary for every trial
-                    trialList += [{
+                    trial_list += [{
                         'ori': ori,
                         'xpos': pos,
                         'fixPos': pos if condition == 'trueFix' else random.choice([x for x in positions if x is not pos]),
@@ -245,65 +243,65 @@ def makeTrialList(condition):
 
     # Randomize order
     from random import sample
-    trialList = sample(trialList, len(trialList))
+    trial_list = sample(trial_list, len(trial_list))
 
     # Add trial numbers and return
-    for i, trial in enumerate(trialList):
+    for i, trial in enumerate(trial_list):
         trial['no'] = i + 1  # start at 1 instead of 0
-    return trialList
+    return trial_list
 
 
-def runBlock(condition):
+def run_block(condition):
     """
     Runs a block of trials. This is the presentation of stimuli,
     collection of responses and saving the trial
     """
-    ask(textInstruct)  # Instruction
+    ask(text_instruct)  # Instruction
 
     # Displays trials. Remember: prepare --> timing critical stuff --> score, save etc.
-    for trial in makeTrialList(condition):
+    for trial in make_trial_list(condition):
         # Prepare trial here, before entering the time-critical period
-        stimGabor.setOri(trial['ori'])
-        stimGabor.setPos([trial['xpos'], 0])
-        stimFix.setPos([trial['fixPos'], 0])
+        stim_gabor.setOri(trial['ori'])
+        stim_gabor.setPos([trial['xpos'], 0])
+        stim_fix.setPos([trial['fixPos'], 0])
 
         # A break at regular interval. Also at the beginning of the experiment
-        # Show break message when trial['no'] is a multiple of pauseInterval (% is modulus)
-        if trial['no'] % pauseInterval is 0:
-            ask(textBreak, keysBreak)
+        # Show break message when trial['no'] is a multiple of pause_interval (% is modulus)
+        if trial['no'] % pause_interval is 0:
+            ask(text_break, keys_break)
 
         # ACTION: THIS IS THE TIMING CRITICAL PART
         # Fixation cue
         win.callOnFlip(clock.reset)
-        for frame in range(framesFix):
-            stimFix.draw()
+        for frame in range(frames_fix):
+            stim_fix.draw()
             win.flip()
 
         # Stimulus
         for frame in range(trial['duration']):
-            stimGabor.draw()
-            stimFix.draw()
+            stim_gabor.draw()
+            stim_fix.draw()
             win.flip()
 
         # Mask
-        for frame in range(framesMask):
+        for frame in range(frames_mask):
             for ori in oris.values():
-                stimGabor.setOri(ori)
-                stimGabor.draw()
-            stimFix.draw()
+                stim_gabor.setOri(ori)
+                stim_gabor.draw()
+            stim_fix.draw()
             win.flip()
 
         # Get actual duration at offset
-        stimFix.draw()
+        stim_fix.draw()
         win.flip()  # blank screen
         trial['durationReal'] = clock.getTime()
 
         # END OF TIMING CRITICAL SECTION
         # Ask question and record responses.
-        stimFix.draw()
+        stim_fix.draw()
         trial['response'], trial['rt'] = ask(' ', oris.keys())
         trial['score'] = 1 if oris[trial['response']] == trial['ori'] else 0  # 1 if key corresponds to the shown orientation
-        beepSuccess.play() if trial['score'] else beepFail.play()  # feedback
+        sound_success.play() if trial['score'] else sound_fail.play()  # feedback
 
         # Save trial
         writer.write(trial)
@@ -312,13 +310,13 @@ def runBlock(condition):
 """
  RUN EXPERIMENT
  Now it's really simple. You simply execute things using the functions ask and
- runBlock. Here we order block types given input from dialogue box
+ run_block. Here we order block types given input from dialogue box
 """
 ask()
 if V['condition'] == 'trueFix':
-    runBlock('trueFix')
-    runBlock('falseFix')
+    run_block('trueFix')
+    run_block('falseFix')
 
 elif V['condition'] == 'falseFix':
-    runBlock('falseFix')
-    runBlock('trueFix')
+    run_block('falseFix')
+    run_block('trueFix')
